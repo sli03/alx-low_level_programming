ab3664c23af49fe95a69a18047941da7c00787d6
Slie <slindilemnisi47@gmail.com>
2023-07-21 13:45:04 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - function
+ * @c: parametre
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
